d39f783bd7c4e2978e92f3d115d522d78938e6f7
Vladimir <vs-kornilov@live.ru>
2019-10-16 22:27:54 +0400

    Редактирование главной страницы. Добовление слайдера и новостей.

---
+++ ViewModels/News.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ws_SushiBar.ViewModels
+{
+    public class News
+    {
+        public string Date { get; set; }
+        public string NewsTitle { get; set; }
+        public string NewsPost { get; set; }
+    }
+}
